5c8d0537bfdd245e8909443728d88a69508b6eb0
Constantine Zykov <zykov@falsecode.ru>
2015-05-14 17:30:16 +1000

    Добавил создание системных папок /tmp/php_sessions и /tmp/php_upload при установке виртуальной машины.

--- .vagrant/update-env.sh
+++ .vagrant/update-env.sh
@@ -8,3 +8,9 @@ mv -f /etc/php.d/xdebug.ini.disabled /etc/php.d/xdebug.ini
 
 echo -e "\e[1;32mRestarting apache service\e[0m"
 service httpd restart
+
+mkdir -p /tmp/php_sessions/www
+mkdir -p /tmp/php_upload/www
+
+chown -R bitrix:bitrix /tmp/php_session
+chown -R bitrix:bitrix /tmp/php_upload
